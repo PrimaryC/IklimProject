98484e4a45fe160c47ea428d2885987d822e0ac2
uosbell@gmail.com <nicervictory@gmail.com>
2017-06-21 02:19:24 +0900

    nurie_redis 0.5v reupload

---
+++ Society/src/iklim/society/interaction/InteractionManager.java
@@ -0,0 +1,14 @@
+package iklim.society.interaction;
+
+import iklim.society.model.ModelManager;
+
+public class InteractionManager {
+
+	ModelManager model;
+	
+	public InteractionManager(ModelManager model) {
+		this.model = model;
+		
+	}
+
+}
